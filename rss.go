2e45b3cef47a6219931ba1c36e75eb6be8d6d9c4
Donald Huang <don.hcd@gmail.com>
2016-01-20 21:04:54 -0800

    add/improve config format

--- rss.go
+++ rss.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	rss "github.com/jteeuwen/go-pkg-rss"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
@@ -24,22 +26,64 @@ func main() {
 func chanHandler(feed *rss.Feed, newchannels []*rss.Channel) {
 }
 
-func itemHandler(feed *rss.Feed, ch *rss.Channel, newitems []*rss.Item) {
-	for _, newItem := range newitems {
-		ciTit := strings.ToLower(newItem.Title)
-		isGood := false
-		for _, wantedSubString := range strings.Split("sfo,san francisco,sjc,san jose,oak,oakland", ",") {
-			if strings.Contains(ciTit, wantedSubString) {
-				isGood = true
+type OredInterests []string
+
+type Interests struct {
+	Email          string
+	AndedInterests []OredInterests
+}
+
+var interests []Interests
+
+func getRecipientsForItem(item *rss.Item) []string {
+	var recipients []string
+	for _, interest := range interests {
+		ciTit := strings.ToLower(item.Title)
+		matchesAnd := true
+		for _, oredInterest := range interest.AndedInterests {
+			matchesOr := false
+			for _, wantedSubstring := range oredInterest {
+				matchesOr = matchesOr || strings.Contains(ciTit, wantedSubstring)
+				if matchesOr {
+					break
+				}
+			}
+			matchesAnd = matchesAnd && matchesOr
+			if !matchesAnd {
+				break
 			}
 		}
-		if !isGood {
-			continue
+		if matchesAnd {
+			recipients = append(recipients, interest.Email)
 		}
-		cmd := exec.Command("mail", "-s", "Flight deal: "+newItem.Title, "don.hcd@gmail.com")
-		cmd.Stdin = bytes.NewReader([]byte(fmt.Sprintf("Link: %s\n\nDescription: %s\n", newItem.Comments, newItem.Description)))
+	}
+	return recipients
+}
+
+func sendEmailForItem(item *rss.Item, email string) {
+	// println(email, item.Title)
+	// _ = exec.Command
+	// _ = bytes.NewReader
+		cmd := exec.Command("mail", "-s", "Flight deal: "+item.Title, email)
+		cmd.Stdin = bytes.NewReader([]byte(fmt.Sprintf("Link: %s\n\nDescription: %s\n", item.Comments, item.Description)))
 		if output, err := cmd.CombinedOutput(); err != nil {
 			fmt.Fprintf(os.Stderr, "[e] mail error: %s; output: %q\n", err, string(output))
 		}
+}
+
+func itemHandler(feed *rss.Feed, ch *rss.Channel, newitems []*rss.Item) {
+	for _, newItem := range newitems {
+		recipients := getRecipientsForItem(newItem)
+		for _, recipient := range recipients {
+			sendEmailForItem(newItem, recipient)
+		}
+	}
+}
+
+func init() {
+	if bytes, err := ioutil.ReadFile("config.json"); err != nil {
+		panic(err)
+	} else if err := json.Unmarshal(bytes, &interests); err != nil {
+		panic(err)
 	}
 }
